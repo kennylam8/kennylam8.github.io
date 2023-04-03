f1870c4d02d968820b2e789b05e0d8052290d9d8
Kenny Lam <kenny@kennylam8.com>
2023-04-04 03:38:33 +1000

    Added the project session (on-going development) and fixed the bug in the skills part

---
+++ skill_chart_2.js
@@ -0,0 +1,121 @@
+    // Initialize the echarts instance based on the prepared dom
+    var chartDom2 = document.getElementById('mainchart2');
+    var myChart2 = echarts.init(chartDom2);
+    var option2;
+    // Specify the configuration items and data for the chart
+
+    function makeOption2(type, symbol) {
+        return {
+            tooltip: {
+                trigger: 'item',
+                axisPointer: {
+                    type: 'shadow'
+                },
+                formatter: function(params) {
+                    return  `<p class="lead">${params.data.tt_name}</p>${params.data.tt_pj} related projects<br/>${params.data.tt_yoe} years of experience<br/>${params.data.tt_ct} related courses taken`;
+                }
+            },
+            grid: {
+                containLabel: true,
+                left: 0,
+                top: 0,
+                // right: 20,
+                bottom: 0
+            },
+            yAxis: {
+                // data: ['Visualization',  "Communication", 'Statistical Analysis', "PyTorch", "Bioinformatics"],
+                data:[{
+                    value: 'Visualization',
+                    // Highlight Monday
+                    textStyle: {
+                        color: '#ffffff'
+                    }
+                }, {
+                    value: 'Communication',
+                    // Highlight Monday
+                    textStyle: {
+                        color: '#ffffff'
+                    }
+                },{
+                    value: 'Statistical Analysis',
+                    // Highlight Monday
+                    textStyle: {
+                        color: '#ffffff'
+                    }
+                },{
+                    value: 'PyTorch',
+                    // Highlight Monday
+                    textStyle: {
+                        color: '#ffffff'
+                    }
+                },{
+                    value: 'Bioinformatics',
+                    // Highlight Monday
+                    textStyle: {
+                        color: '#ffffff'
+                    }
+                },],
+                inverse: true,
+                // type: 'category',
+                axisLine: { show: false },
+                axisTick: { show: false },
+                axisLabel: {
+                    margin: 18,
+                    fontSize: 14
+                },
+                axisPointer: {
+                    label: {
+                        show: true,
+                        margin: 30
+                    }
+                }
+            },
+            xAxis: {
+                splitLine: { show: false },
+                axisLabel: { show: false },
+                axisTick: { show: false },
+                axisLine: { show: false },
+                // type: 'value'
+            },
+            animationDurationUpdate: 1000,
+            series: [
+                {
+                    id: 'bar1',
+                    type: type,
+                    label: labelSetting,
+                    stack: 'total',
+                    emphasis: {
+                        focus: 'series'
+                    },
+                    showBackground: true,
+                    symbolRepeat: true,
+                    symbolSize: ['80%', '75%'],
+                    barCategoryGap: '40%',
+                    universalTransition: {
+                        enabled: true,
+                        delay: function (idx, total) {
+                            return (idx / total) * 1000;
+                        }
+                    },data: [{value: 100, symbol: symbol || pathSymbols.visualisation, tt_name:"Visualisation", tt_pj:"20", tt_yoe:"5", tt_ct:"12"}, // Visualisation
+                        {value: 90, symbol: symbol || pathSymbols.communication, tt_name:"Communication", tt_pj:"4", tt_yoe:"2", tt_ct:"3"}, // Communication
+                        {value: 80, symbol: symbol || pathSymbols.statAnalysis, tt_name:"Statistical Analysis", tt_pj:"3", tt_yoe:"2", tt_ct:"3"}, // Statistical Analysis
+                        {value: 70, symbol: symbol || pathSymbols.torch, tt_name:"PyTorch", tt_pj:"6", tt_yoe:"2", tt_ct:"3"}, // PyTorch
+                        {value: 70, symbol: symbol || pathSymbols.binf, tt_name:"Bioinformatics", tt_pj:"4", tt_yoe:"3", tt_ct:"4"}, // Bioinformatics
+                        // {value: 60, symbol: symbol || pathSymbols.python, tt_name:"Haskell", tt_pj:"3", tt_yoe:"1", tt_ct:"1"}, //
+                    // {value: 60, symbol: symbol || pathSymbols.python, tt_name:"Haskell", tt_pj:"3", tt_yoe:"1", tt_ct:"1"},
+                    ] // Haskell
+
+                },
+
+            ]
+        };
+    }
+    var options2 = [
+        makeOption2('pictorialBar'),
+        makeOption2('bar'),
+    ];
+    var optionIndex2 = 0;
+    option2 = options2[optionIndex2];
+
+    // Display the chart using the configuration items and data just specified.
+    myChart2.setOption(option2);
